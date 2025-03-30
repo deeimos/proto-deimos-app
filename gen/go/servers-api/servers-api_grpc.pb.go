8fbae47b7b320780c23da994245a5f2c60a2be55
deimos <dmsnellen@gmail.com>
2025-03-30 18:52:39 +0300

    update servers-api

--- gen/go/servers-api/servers-api_grpc.pb.go
+++ gen/go/servers-api/servers-api_grpc.pb.go
@@ -19,177 +19,215 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	ServersService_SaveServer_FullMethodName     = "/servers_api.ServersService/SaveServer"
-	ServersService_GetServer_FullMethodName      = "/servers_api.ServersService/GetServer"
-	ServersService_GetServersList_FullMethodName = "/servers_api.ServersService/GetServersList"
+	ServersAPI_CreateServer_FullMethodName   = "/servers_api.ServersAPI/CreateServer"
+	ServersAPI_UpdateServer_FullMethodName   = "/servers_api.ServersAPI/UpdateServer"
+	ServersAPI_GetServer_FullMethodName      = "/servers_api.ServersAPI/GetServer"
+	ServersAPI_GetServersList_FullMethodName = "/servers_api.ServersAPI/GetServersList"
 )
 
-// ServersServiceClient is the client API for ServersService service.
+// ServersAPIClient is the client API for ServersAPI service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type ServersServiceClient interface {
-	SaveServer(ctx context.Context, in *SaveServerRequest, opts ...grpc.CallOption) (*SaveServerResponse, error)
+type ServersAPIClient interface {
+	CreateServer(ctx context.Context, in *CreateServerRequest, opts ...grpc.CallOption) (*CreateServerResponse, error)
+	UpdateServer(ctx context.Context, in *UpdateServerRequest, opts ...grpc.CallOption) (*UpdateServerResponse, error)
 	GetServer(ctx context.Context, in *GetServerRequest, opts ...grpc.CallOption) (*GetServerResponse, error)
 	GetServersList(ctx context.Context, in *GetServersListRequest, opts ...grpc.CallOption) (*GetServersListResponse, error)
 }
 
-type serversServiceClient struct {
+type serversAPIClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewServersServiceClient(cc grpc.ClientConnInterface) ServersServiceClient {
-	return &serversServiceClient{cc}
+func NewServersAPIClient(cc grpc.ClientConnInterface) ServersAPIClient {
+	return &serversAPIClient{cc}
 }
 
-func (c *serversServiceClient) SaveServer(ctx context.Context, in *SaveServerRequest, opts ...grpc.CallOption) (*SaveServerResponse, error) {
+func (c *serversAPIClient) CreateServer(ctx context.Context, in *CreateServerRequest, opts ...grpc.CallOption) (*CreateServerResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(SaveServerResponse)
-	err := c.cc.Invoke(ctx, ServersService_SaveServer_FullMethodName, in, out, cOpts...)
+	out := new(CreateServerResponse)
+	err := c.cc.Invoke(ctx, ServersAPI_CreateServer_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *serversServiceClient) GetServer(ctx context.Context, in *GetServerRequest, opts ...grpc.CallOption) (*GetServerResponse, error) {
+func (c *serversAPIClient) UpdateServer(ctx context.Context, in *UpdateServerRequest, opts ...grpc.CallOption) (*UpdateServerResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(UpdateServerResponse)
+	err := c.cc.Invoke(ctx, ServersAPI_UpdateServer_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *serversAPIClient) GetServer(ctx context.Context, in *GetServerRequest, opts ...grpc.CallOption) (*GetServerResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(GetServerResponse)
-	err := c.cc.Invoke(ctx, ServersService_GetServer_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, ServersAPI_GetServer_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *serversServiceClient) GetServersList(ctx context.Context, in *GetServersListRequest, opts ...grpc.CallOption) (*GetServersListResponse, error) {
+func (c *serversAPIClient) GetServersList(ctx context.Context, in *GetServersListRequest, opts ...grpc.CallOption) (*GetServersListResponse, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(GetServersListResponse)
-	err := c.cc.Invoke(ctx, ServersService_GetServersList_FullMethodName, in, out, cOpts...)
+	err := c.cc.Invoke(ctx, ServersAPI_GetServersList_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// ServersServiceServer is the server API for ServersService service.
-// All implementations must embed UnimplementedServersServiceServer
+// ServersAPIServer is the server API for ServersAPI service.
+// All implementations must embed UnimplementedServersAPIServer
 // for forward compatibility.
-type ServersServiceServer interface {
-	SaveServer(context.Context, *SaveServerRequest) (*SaveServerResponse, error)
+type ServersAPIServer interface {
+	CreateServer(context.Context, *CreateServerRequest) (*CreateServerResponse, error)
+	UpdateServer(context.Context, *UpdateServerRequest) (*UpdateServerResponse, error)
 	GetServer(context.Context, *GetServerRequest) (*GetServerResponse, error)
 	GetServersList(context.Context, *GetServersListRequest) (*GetServersListResponse, error)
-	mustEmbedUnimplementedServersServiceServer()
+	mustEmbedUnimplementedServersAPIServer()
 }
 
-// UnimplementedServersServiceServer must be embedded to have
+// UnimplementedServersAPIServer must be embedded to have
 // forward compatible implementations.
 //
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
-type UnimplementedServersServiceServer struct{}
+type UnimplementedServersAPIServer struct{}
 
-func (UnimplementedServersServiceServer) SaveServer(context.Context, *SaveServerRequest) (*SaveServerResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SaveServer not implemented")
+func (UnimplementedServersAPIServer) CreateServer(context.Context, *CreateServerRequest) (*CreateServerResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateServer not implemented")
 }
-func (UnimplementedServersServiceServer) GetServer(context.Context, *GetServerRequest) (*GetServerResponse, error) {
+func (UnimplementedServersAPIServer) UpdateServer(context.Context, *UpdateServerRequest) (*UpdateServerResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateServer not implemented")
+}
+func (UnimplementedServersAPIServer) GetServer(context.Context, *GetServerRequest) (*GetServerResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetServer not implemented")
 }
-func (UnimplementedServersServiceServer) GetServersList(context.Context, *GetServersListRequest) (*GetServersListResponse, error) {
+func (UnimplementedServersAPIServer) GetServersList(context.Context, *GetServersListRequest) (*GetServersListResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetServersList not implemented")
 }
-func (UnimplementedServersServiceServer) mustEmbedUnimplementedServersServiceServer() {}
-func (UnimplementedServersServiceServer) testEmbeddedByValue()                        {}
+func (UnimplementedServersAPIServer) mustEmbedUnimplementedServersAPIServer() {}
+func (UnimplementedServersAPIServer) testEmbeddedByValue()                    {}
 
-// UnsafeServersServiceServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to ServersServiceServer will
+// UnsafeServersAPIServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to ServersAPIServer will
 // result in compilation errors.
-type UnsafeServersServiceServer interface {
-	mustEmbedUnimplementedServersServiceServer()
+type UnsafeServersAPIServer interface {
+	mustEmbedUnimplementedServersAPIServer()
 }
 
-func RegisterServersServiceServer(s grpc.ServiceRegistrar, srv ServersServiceServer) {
-	// If the following call pancis, it indicates UnimplementedServersServiceServer was
+func RegisterServersAPIServer(s grpc.ServiceRegistrar, srv ServersAPIServer) {
+	// If the following call pancis, it indicates UnimplementedServersAPIServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
 	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
 		t.testEmbeddedByValue()
 	}
-	s.RegisterService(&ServersService_ServiceDesc, srv)
+	s.RegisterService(&ServersAPI_ServiceDesc, srv)
 }
 
-func _ServersService_SaveServer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(SaveServerRequest)
+func _ServersAPI_CreateServer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateServerRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ServersServiceServer).SaveServer(ctx, in)
+		return srv.(ServersAPIServer).CreateServer(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: ServersService_SaveServer_FullMethodName,
+		FullMethod: ServersAPI_CreateServer_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServersServiceServer).SaveServer(ctx, req.(*SaveServerRequest))
+		return srv.(ServersAPIServer).CreateServer(ctx, req.(*CreateServerRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ServersService_GetServer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _ServersAPI_UpdateServer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateServerRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ServersAPIServer).UpdateServer(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: ServersAPI_UpdateServer_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ServersAPIServer).UpdateServer(ctx, req.(*UpdateServerRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _ServersAPI_GetServer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetServerRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ServersServiceServer).GetServer(ctx, in)
+		return srv.(ServersAPIServer).GetServer(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: ServersService_GetServer_FullMethodName,
+		FullMethod: ServersAPI_GetServer_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServersServiceServer).GetServer(ctx, req.(*GetServerRequest))
+		return srv.(ServersAPIServer).GetServer(ctx, req.(*GetServerRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _ServersService_GetServersList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _ServersAPI_GetServersList_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetServersListRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ServersServiceServer).GetServersList(ctx, in)
+		return srv.(ServersAPIServer).GetServersList(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: ServersService_GetServersList_FullMethodName,
+		FullMethod: ServersAPI_GetServersList_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ServersServiceServer).GetServersList(ctx, req.(*GetServersListRequest))
+		return srv.(ServersAPIServer).GetServersList(ctx, req.(*GetServersListRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// ServersService_ServiceDesc is the grpc.ServiceDesc for ServersService service.
+// ServersAPI_ServiceDesc is the grpc.ServiceDesc for ServersAPI service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var ServersService_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "servers_api.ServersService",
-	HandlerType: (*ServersServiceServer)(nil),
+var ServersAPI_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "servers_api.ServersAPI",
+	HandlerType: (*ServersAPIServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "SaveServer",
-			Handler:    _ServersService_SaveServer_Handler,
+			MethodName: "CreateServer",
+			Handler:    _ServersAPI_CreateServer_Handler,
+		},
+		{
+			MethodName: "UpdateServer",
+			Handler:    _ServersAPI_UpdateServer_Handler,
 		},
 		{
 			MethodName: "GetServer",
-			Handler:    _ServersService_GetServer_Handler,
+			Handler:    _ServersAPI_GetServer_Handler,
 		},
 		{
 			MethodName: "GetServersList",
-			Handler:    _ServersService_GetServersList_Handler,
+			Handler:    _ServersAPI_GetServersList_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
